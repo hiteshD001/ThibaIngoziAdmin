d53e900b4964a7108c997449a76853b059edafd1
hiteshD01 <hitesh.crestcoder@gmail.com>
2024-07-27 13:42:40 +0530

    feat: add request-hardware page design

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import "./App.css";
 // import Layout from "./common/Layout";
 import { Login } from "./pages/Login";
 import ResetPassword from "./pages/ResetPassword";
+import RequestHardware from "./pages/RequestHardware";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/reset-password" element={<ResetPassword/>}/>
-        {/* <Route path="/" element={<Layout />}>
-        </Route> */}
+        <Route path="/" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/request-hardware" element={<RequestHardware />}></Route>
       </Routes>
     </>
   );
---
+++ src/pages/RequestHardware.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "../css/RequestHardware.css";
+import sosHardware from "../assets/images/soshardware.png";
+
+const RequestHardware = () => {
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(100);
+  console.log("setPricezz", setPrice);
+
+  return (
+    <div className="reqhardware-container">
+      <div className="wrapper">
+        <h3 className="heading">Request Hardware</h3>
+        <img src={sosHardware} />
+        <span>
+          <p className="title">Name</p>
+          <h2 className="name">Kenneth C. King</h2>
+        </span>
+        <span>
+          <p className="title">Delivery Address</p>
+          <p className="address">
+            Alameda dos Palmitos, 332, <br /> Randburg, Johannesburg
+          </p>
+        </span>
+
+        <div className="price-container">
+          <div className="quantity">
+            <p onClick={() => setQuantity((p) => p - 1)}>-</p>
+            <p>{quantity}</p>
+            <p onClick={() => setQuantity((p) => p + 1)}>+</p>
+          </div>
+
+          <h1 className="price">{quantity * price}</h1>
+        </div>
+
+        <form action="https://sandbox.payfast.co.za/eng/process" method="post">
+          <input type="hidden" name="merchant_id" value="YOUR_MERCHANT_ID" />
+          <input type="hidden" name="merchant_key" value="YOUR_MERCHANT_KEY" />
+          <input type="hidden" name="return_url" value="YOUR_RETURN_URL" />
+          <input type="hidden" name="cancel_url" value="YOUR_CANCEL_URL" />
+          <input type="hidden" name="notify_url" value="YOUR_NOTIFY_URL" />
+          <input type="hidden" name="amount" value={quantity * price} />
+          <input type="hidden" name="item_name" value="Test Item" />
+          <button className="paybutton" type="submit">
+            Pay Now
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RequestHardware;
