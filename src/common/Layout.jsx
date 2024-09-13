321052d27b97464c3fbbf4822179cd64e5807f96
divyeshfefar <divyesh.crestcoder@gmail.com>
2024-09-13 14:41:26 +0530

    update responsive issues

--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -1,24 +1,33 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import toggles from "../assets/images/bar.png";
 import SideBar from "./SideBar";
+import React from "react";
+
+
+    
 
 
 
-
-function Layout() {
-    return (
-        <div className="dashboard">
-            <div className="main-app">
-                <SideBar />
-                <div className="content">
-                    <Navbar />
-                    <div className="outlet">
-                        <Outlet />
+    function Layout() {
+        const [isActive, setActive] = React.useState(false);
+        return (
+            <div className={`dashboard ${isActive ? "active" : ""}`}>
+                <div className="toggle">
+                    <a href="#" onClick={() => setActive(!isActive)}><img src={toggles} alt="toggles" /></a>
+                    
+                </div>
+                <div className="main-app">
+                    <SideBar />
+                    <div className="content">
+                        
+                        <Navbar />
+                        <div className="outlet">
+                            <Outlet />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
-
+        );
+    }
 export default Layout;
