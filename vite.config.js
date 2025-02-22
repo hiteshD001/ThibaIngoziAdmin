3765207187e47f084c51282d261897d439bd4e88
Ubuntu <thiba@thiba-ingozi.pw5kalj3ps1ete4opecorova4b.jnbx.internal.cloudapp.net>
2025-02-22 07:32:43 +0000

    permission change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.xlsx'],
-  plugins: [react()]
+  plugins: [react()],
+  build: { 
+	outDir: 'build',
+  }
 })
