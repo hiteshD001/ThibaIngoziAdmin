87c657f1514da8d7b6c134e3497b8e618a4bde0e
dodiyah330 <dodiyah330@gmail.com>
2026-03-12 16:56:18 +0530

    fix other user redirection

--- src/pages/e-hailing view/EHailingView.jsx
+++ src/pages/e-hailing view/EHailingView.jsx
@@ -665,7 +665,7 @@ const EHialingView = ({ isMapLoaded }) => {
                                                                         alignItems="center"
                                                                         borderRadius={1}
                                                                         onClick={() =>
-                                                                            nav(`total-drivers/driver-information/${user?.otherUser?._id}`)
+                                                                            nav(`/home/total-drivers/driver-information/${user?.otherUser?._id}`, { replace: true })
                                                                         }
                                                                     >
                                                                         Other User
@@ -1003,7 +1003,7 @@ const EHialingView = ({ isMapLoaded }) => {
                                                                                 "&:hover": { backgroundColor: "#1864c7" },
                                                                             }}
                                                                             onClick={() =>
-                                                                                nav(`total-drivers/driver-information/${row?.otherUser?._id}`)
+                                                                                nav(`/home/total-drivers/driver-information/${row?.otherUser?._id}`, { replace: true })
                                                                             }
                                                                         >
                                                                             Other User
